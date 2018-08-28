131e1ed06f71151c1291030fa1807569030da15e
Vlad <vladfriends@yahoo.ca>
2018-08-28 18:51:52 +0200

    S4L132: AI controller now finds player in BeginPlay

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,26 +1,40 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "Engine/World.h"
+#include "Engine/Classes/GameFramework/PlayerController.h"
 #include "BattleTank.h"
 #include "TankAIController.h"
 
 void ATankAIController::BeginPlay() {
 	Super::BeginPlay();
 
-	auto tank = GetControlledTank();
+	auto ThisTank = GetControlledTank();
+	auto PlayerTank = GetPlayerTank();
+
+	if (ThisTank)
+		UE_LOG(LogTemp, Warning, TEXT("Player tank is: %s"), *ThisTank->GetName())
+
+	if(PlayerTank)
+		UE_LOG(LogTemp, Warning, TEXT("Player tank is: %s"), *PlayerTank->GetName())
 }
 
 ATank* ATankAIController::GetControlledTank() const {
 	ATank* ControlledTank = Cast<ATank>(GetPawn());
 
-	if (ControlledTank) {
-		UE_LOG(LogTemp, Warning, TEXT("AI Controller tank name: %s"), *ControlledTank->GetName())
-	}
-
-	else {
-		UE_LOG(LogTemp, Warning, TEXT("No player controller tank found!!"))
+	if(!ControlledTank){
+		UE_LOG(LogTemp, Warning, TEXT("No AI controller tank found!!"))
 	}
 
 	return ControlledTank;
 }
 
+ATank* ATankAIController::GetPlayerTank() const {
+	ATank* PlayerTank = Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+
+	if (!PlayerTank) {
+		UE_LOG(LogTemp, Warning, TEXT("No player tank found!"))
+	}
+
+	return PlayerTank;
+}
 

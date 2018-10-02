069b9956fb0f3834f72afc35f4d753fe837feb7e
Vlad <vladfriends@yahoo.ca>
2018-10-02 19:53:55 +0200

    S4L170: Tanks now move forward and backward according to the dot product between then and the player tank!

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -4,16 +4,23 @@
 #include "TankMovementComponent.h"
 #include "TankTrack.h"
 
-//won't be calling super since we're completely overriding the method
-void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) {
-
-	UE_LOG(LogTemp, Warning, TEXT("%s MoveVelocity: %s"), *GetOwner()->GetName(), *MoveVelocity.ToString())
-}
-
 void UTankMovementComponent::InitialiseProperties(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet) {
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
+
+//won't be calling super since we're completely overriding the method
+void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) {
+
+	auto AiForwardIntention = MoveVelocity.GetSafeNormal();
+	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+	auto ForwardThrow = FVector::DotProduct(AiForwardIntention, TankForward);
+	
+	UE_LOG(LogTemp, Warning, TEXT("%s dot product: %f"), *GetOwner()->GetName(), ForwardThrow)
+
+	IntendMoveForward(ForwardThrow);
+}
+
 
 void UTankMovementComponent::IntendMoveForward(float Throw) {
 	if (!LeftTrack || !RightTrack) { return; }
